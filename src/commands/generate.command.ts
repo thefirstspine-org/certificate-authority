815e2c0350f5f85259da95e95c3c3b544fea6351
The First Spine <59167793+thefirstspine@users.noreply.github.com>
2023-09-11 15:37:53 +0000

    Overall update

--- src/commands/generate.command.ts
+++ src/commands/generate.command.ts
@@ -1,5 +1,3 @@
-import inquirer from 'inquirer';
-import chalk from 'chalk';
 import * as crypto from 'crypto';
 import * as fs from 'fs';
 import validatorService from './../service/validator.service';
@@ -9,17 +7,6 @@ import validatorService from './../service/validator.service';
  * Generates files to be validated in the services.
  */
 export class GenerateCommand {
-
-  /**
-   * Force mode.
-   */
-  protected force: boolean = false;
-
-  /**
-   * The mode of generation to use.
-   */
-  protected mode: string = 'ask';
-
   constructor() {}
 
   /**
@@ -27,43 +14,18 @@ export class GenerateCommand {
    * @param args The arguments of the command
    */
   async start(args: IGenerateCommandArgs | (IGenerateCommandArgs & IGenerateCommandPairsArgs)) {
-    console.log(chalk.blue(`Certification generation toolkit`));
-
-    // Validate arguments
-    if (args.force) {
-      console.log(chalk.yellowBright('Using `force` mode. Hope you know what you are doing.'));
-      this.force = true;
-    }
-    this.mode = args.mode;
-
-    // Getting mode
-    if (this.mode === 'ask') {
-      const menuResponses = await inquirer.prompt([
-        {
-          type: 'list',
-          name: 'menu',
-          message: 'What do you want to do?',
-          choices: [
-            'Generate a new pair',
-          ],
-        }
-      ]);
-      if (menuResponses.menu === 'Generate a new pair') {
-        this.mode = 'pair';
-      } else if (menuResponses.menu === 'Generate a new certificate from a private key') {
-      }
-    }
+    console.log(`Certification generation toolkit`);
 
     // Validating mode
     const modesMap = {
       'pair': this.generatePair.bind(this),
     };
-    if (!modesMap[this.mode]) {
-      console.log(chalk.redBright(`Unhandled mode "${this.mode}"`));
+    if (!modesMap[args.mode]) {
+      console.log(`Unhandled mode "${args.mode}"`);
       process.exit(1);
     }
 
-    modesMap[this.mode](args);
+    modesMap[args.mode](args);
   }
 
   /**
@@ -71,40 +33,22 @@ export class GenerateCommand {
    * @param args
    */
   async generatePair(args: IGenerateCommandPairsArgs) {
-    // Getting key
-    const key: string = await this.promptForValue('Where should be located your PRIVATE key file?', args, 'key');
-    if (fs.existsSync(key)) {
-      const responses = this.force ? {overwrite: true} : await inquirer.prompt([
-        {
-          type: 'confirm',
-          name: 'overwrite',
-          message: `${key} already exists. Overwrite?`,
-        }
-      ]);
-      if (!responses.overwrite) {
-        process.exit(1);
-      }
-      fs.unlinkSync(key);
-    }
+    // Getting args
+    const key: string|undefined = args.key;
+    const certificate: string|undefined = args.certificate;
 
-    // Getting certificate
-    const certificate: string = await this.promptForValue('Where should be located your PUBLIC certificate file?', args, 'certificate');
-    if (fs.existsSync(certificate)) {
-      const responses = this.force ? {overwrite: true} : await inquirer.prompt([
-        {
-          type: 'confirm',
-          name: 'overwrite',
-          message: `${certificate} already exists. Overwrite?`,
-        }
-      ]);
-      if (!responses.overwrite) {
-        process.exit(1);
-      }
-      fs.unlinkSync(certificate);
+    // Validate args
+    if (key == undefined) {
+      console.log('"[k]ey" is required.');
+      process.exit(1);
+    }
+    if (certificate == undefined) {
+      console.log('"[c]ertificate" is required.');
+      process.exit(1);
     }
 
     // Generating pair
-    console.log(chalk.blueBright('Generating pair...'));
+    console.log('Generating pair...');
     const result = crypto.generateKeyPairSync(
       'rsa',
       {
@@ -122,47 +66,28 @@ export class GenerateCommand {
 
     // We have to test against our validation service
     // This will prevent to have false certificates to block the services
-    console.log(chalk.blueBright('Testing pair...'));
+    console.log('Testing pair...');
     process.env.PRIVATE_KEY = result.privateKey;
     const challengeResult = validatorService.challenge(result.publicKey);
     if (!challengeResult) {
-      console.log(chalk.redBright(`Challenge failed.`));
+      console.log(`Challenge failed.`);
       process.exit(1);
     }
-    console.log(chalk.greenBright('Challenge succeed.'));
+    console.log('Challenge succeed.');
     
     // Write the ertificates
     fs.writeFileSync(key, result.privateKey);
-    console.log(chalk.greenBright(`Generated key in ${key}`));
+    console.log(`Generated key in ${key}`);
     fs.writeFileSync(certificate, result.publicKey);
-    console.log(chalk.greenBright(`Generated certificate in ${certificate}`));
+    console.log(`Generated certificate in ${certificate}`);
 
     // ..and we're done!
-    console.log(chalk.greenBright(`done.`));
+    console.log(`done.`);
     process.exit();
   }
-
-  /**
-   * Ask for the user some input according to a key in an arguments list.
-   * @param message
-   * @param args
-   * @param key
-   */
-  protected async promptForValue(message: string, args: any, key: string): Promise<string> {
-    const response = args[key] ? {[key]: args[key]} : await inquirer.prompt([
-      {
-        type: 'input',
-        name: key,
-        message,
-      }
-    ]);
-    return response[key];
-  }
-
 }
 
 export interface IGenerateCommandArgs {
-  force: boolean|undefined;
   mode: string|undefined;
 }
 
--- src/commands/generate.ts
+++ src/commands/generate.ts
@@ -1,47 +1,19 @@
 #!/usr/bin/env node
 
 import { GenerateCommand } from "./generate.command";
-import yargs from 'yargs';
-
-// Clear the console
-const clear = require('clear');
-clear();
-
-yargs
-  .command('', 'Generates files to be validated in the services.')
-  .options({
-    force: {
-      alias: 'f',
-      type: 'boolean',
-      description: 'Once provided, the confirmation prompt will be ignored.',
-      default: false,
-    },
-    mode: {
-      alias: 'm',
-      type: 'string',
-      description: 'Defines wha\'s to be generated. `"ask"` or `"pair"`. If not provided, a prompt will be displayed.',
-      default: 'ask',
-    },
-    key: {
-      alias: 'k',
-      type: 'string',
-      description: 'The private key path. If not provided, a prompt will be displayed. If the file already exists, a confirmation will be asked',
-      default: false,
-    },
-    certificate: {
-      alias: 'c',
-      type: 'string',
-      description: 'The public certificate path. If not provided, a prompt will be displayed. If the file already exists, a confirmation will be asked',
-      default: false,
-    },
-  });
 
 // Bootstrap the command
 const command: GenerateCommand = new GenerateCommand();
-const args = yargs.parse();
+
+// Get options
+var argv = require('minimist')(process.argv.slice(2));
+if (argv['m'] == undefined) {
+  console.log('-[m]ode option is missing');
+  process.exit(1);
+}
+
 command.start({
-  force: args.force as boolean,
-  mode: args.mode as string,
-  key: args.key as string,
-  certificate: args.certificate as string,
+  mode: argv['m'],
+  key: argv['k'],
+  certificate: argv['c'],
 });
